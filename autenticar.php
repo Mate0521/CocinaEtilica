922b3cd57e41cf97402ea49ea9c7e48555293c73
Hector Florez <hectorarturo@yahoo.com>
2025-09-24 15:49:14 -0500

    Autenticacion
    Autenticación del administrador

---
+++ autenticar.php
@@ -0,0 +1,69 @@
+<?php 
+session_start();
+require_once ("logica/Persona.php");
+require_once ("logica/Admin.php");
+require_once ("logica/Cliente.php");
+
+if(isset($_POST["autenticar"])){
+    $correo = $_POST["correo"];
+    $clave = $_POST["clave"];
+    $admin = new Admin("", "", "", $correo, $clave);
+    if($admin -> autenticar()){
+        $_SESSION["id"] = $admin -> getId();
+        echo "autentico";
+        header('Location: sesionAdmin.php');
+    }
+    
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Cocina Etilica</title>
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+	rel="stylesheet">
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row mt-5">
+			<div class="col-4"></div>
+			<div class="col-4">
+				<div class="card">
+					<div class="card-header">
+						<h3>Autenticar</h3>
+					</div>
+					<div class="card-body">
+						<?php 
+						if(isset($_POST["registrar"])){
+						    echo "<div class='alert alert-success' role='alert'>
+                                    Cliente almacenado
+                                    </div>";
+						}
+						?>
+						<form method="post" action="autenticar.php">
+							<div class="mb-3">
+								<input type="email" class="form-control" name="correo"
+									placeholder="Correo" required>
+							</div>
+							<div class="mb-3">
+								<input type="password" class="form-control" name="clave"
+									placeholder="Clave" required>
+							</div>
+							<div class="mb-3">
+								<button type="submit" class="btn btn-primary" name="autenticar">Autenticar</button>
+							</div>
+
+						</form>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
